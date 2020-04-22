3bcd5a6e4e3a5d9f60ee982edbb4aae8988eaf03
Ksenia <kseniia@Andreys-MacBook-Air.local>
2020-04-22 03:01:28 +0100

    about deploy

--- my_site/settings.py
+++ my_site/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 import dj_database_url
 
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('HEROKU_POSTGRESQL_CRIMSON_URL'))}
+# DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
