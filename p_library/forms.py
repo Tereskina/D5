edd73273ec06984994f5672224b7319e83f429c0
Ksenia <kseniia@Andreys-MacBook-Air.local>
2020-04-22 02:16:29 +0100

    initial commit

---
+++ p_library/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from p_library.models import Author, Book, Redaction
+from django.forms import formset_factory 
+
+class AuthorForm(forms.ModelForm):
+    full_name = forms.CharField(widget=forms.TextInput)
+    class Meta:
+        fields = "__all__"
+        model = Author
+
+    
+class BookForm(forms.ModelForm):
+    class Meta:
+        fields="__all__"
+        model=Book
